d744990aa6f60106f9a05273cb3cac5817513bf4
Dudkin Timofey <dudkinta@gmail.com>
2024-07-12 12:45:23 +0300

    add AppModule

--- FrontApp/src/main.ts
+++ FrontApp/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppModule, appConfig)
   .catch((err) => console.error(err));
